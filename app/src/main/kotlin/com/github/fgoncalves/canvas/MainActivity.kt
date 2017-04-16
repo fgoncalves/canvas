51848a7767f6082c7e52b19d39ea70c88f2a12e6
Fred <fgoncalves@babbel.com>
2017-04-16 12:50:31 +0200

    Adds layout for large screens

--- app/src/main/kotlin/com/github/fgoncalves/canvas/MainActivity.kt
+++ app/src/main/kotlin/com/github/fgoncalves/canvas/MainActivity.kt
@@ -3,7 +3,11 @@ package com.github.fgoncalves.canvas
 import android.animation.ObjectAnimator
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.view.WindowManager
 import com.github.fgoncalves.canvas.databinding.ActivityMainBinding
 import com.github.fgoncalves.canvas.di.ApplicationComponent
@@ -12,6 +16,7 @@ import com.github.fgoncalves.canvas.di.PresentationModule
 
 
 class MainActivity : AppCompatActivity() {
+    private var toggle: ActionBarDrawerToggle? = null
     private val applicationComponent: ApplicationComponent? = CanvasApplication.component
     private var binding: ActivityMainBinding? = null
     private var _presentationComponent: PresentationComponent? = null
@@ -32,7 +37,30 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel.onCreate()
         binding.viewModel.onColorPicked(this::setStatusBarColor)
 
+        val toolbar: Toolbar? = findViewById(R.id.toolbar) as Toolbar?
+        if (toolbar != null) setSupportActionBar(toolbar)
+
+        val drawer: DrawerLayout? = findViewById(R.id.swatches_drawer) as DrawerLayout?
+        if (drawer != null) {
+            toggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+            drawer.addDrawerListener(toggle!!)
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
         this.binding = binding
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle?.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle?.onOptionsItemSelected(item) == true)
+            return true
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
